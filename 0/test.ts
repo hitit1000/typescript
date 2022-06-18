f3a44acaf4ebe42444b5787689f034c579b6ac5d
hitit <hitit1000@naver.com>
2022-06-18 17:44:03 +0900

    add example1.ts

--- 0/test.ts
+++ 0/test.ts
@@ -8,3 +8,13 @@ type Add = (a: number, b: number) => number;
 // 콜 시그니처에 제네릭을 입력하면 어떤 타입의 데이터든 들어올수 있음.
 
 // class / oop ( 객체지향 프로그래밍 )
+
+// abstract class => 추상 클래스
+// 추상 클래스는 인터턴스 할 수없으며, 오로지 하위 클래스 상속을 위한 전용 클래스
+// 변수 선언은 private, public, protected 등으로 사용
+// 상속 된 클래스 내부에서 변수를 접근하고 싶을때 protected 선언
+// private, public 는 스킵
+
+// abstract method => 추상 메소드
+// 추상 메소드는 추상 클래스에서 생상하며, 콜 시그니처 형식으로 선언
+// 모든 하위 클래스는 추상 메소드를 정의해야함
