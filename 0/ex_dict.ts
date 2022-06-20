1ffcea8be10597ffa6040d8292e595e00c4d435c
hitit1000 <hitit1000@naver.com>
2022-06-20 10:48:46 +0900

    1-typescript blockchain

---
+++ 0/ex_dict.ts
@@ -0,0 +1,58 @@
+type Words = {
+  [key: string]: string;
+};
+
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
+    }
+  }
+  def(term: string) {
+    return this.words[term];
+  }
+  upd(word: Word) {
+    if (this.words[word.term]) {
+      this.words[word.term] = word.def;
+      console.log("update completed");
+    } else {
+      console.log("update failed - non-existent word");
+    }
+  }
+  del(term: string) {
+    if (this.words[term]) {
+      delete this.words[term];
+      console.log("delete completed");
+    } else {
+      console.log("delete failed - non-existent word");
+    }
+  }
+}
+
+class Word {
+  constructor(public term: string, public def: string) {}
+}
+
+const dict = new Dict();
+const kimchi = new Word("kimchi", "한국의 반찬");
+const kimbab = new Word("kimbab", "한국의 밥");
+
+dict.add(kimchi);
+dict.add(kimbab);
+
+console.log(dict.def("kimchi"));
+console.log(dict.def("kimbab"));
+
+const kimchi2 = new Word("kimchi", "한국 최고의 반찬");
+
+dict.upd(kimchi2);
+
+console.log(dict.def("kimchi"));
+
+dict.del("kimchi");
+
+// 단어 삭제, 단어 업데이트, 추상클래스 사용
--- 0/study.ts
+++ 0/study.ts
@@ -19,11 +19,11 @@ type Add = (a: number, b: number) => number;
 // 추상 메소드는 추상 클래스에서 생상하며, 콜 시그니처 형식으로 선언
 // 모든 하위 클래스는 추상 메소드를 정의해야함
 
-// Type, interface 
+// Type, interface
 // Type은 유연하게 많은 데이터 타입을 정의할 수 있음
 // Interface 는 오브젝트의 형식을 정의하는 경우에만 사용이 가능
 // Interface 는 동일한 이름의 객체를 여러번 호출하여 정의를 중첩할 수 있음
-// Type은 상속을 &로 표현하며, Interface는 클래스의 상속과 같이 사용 
+// Type은 상속을 &로 표현하며, Interface는 클래스의 상속과 같이 사용
 
 // 추상 클래스 보다 인터페이스를 이용하여 클래스를 생성하는 방법이 더 가볍다
 // interface User{
@@ -32,9 +32,11 @@ type Add = (a: number, b: number) => number;
 // }
 // class Player implements User{
 //   constructor(
-//     public firstName: string    
+//     public firstName: string
 //   ){}
 //   sayHi(name: string) {
 //     return name
 //   }
 // }
+
+// 다형성, 제네릭, 클래스, 인터페이스;
