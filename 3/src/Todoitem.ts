949fa6bdffadb78a127d8b2572c182db545b536f
hitit1000 <hitit1000@naver.com>
2022-06-23 17:48:35 +0900

    update 3_Todolist

---
+++ 3/src/Todoitem.ts
@@ -0,0 +1,15 @@
+interface itemRule {
+  id: number;
+  task: string;
+  complete: boolean;
+  printDetails(complete: boolean): void;
+}
+
+class TodoItem implements itemRule {
+  constructor(readonly id: number, readonly task: string, public complete: boolean) {}
+  public printDetails(): void {
+    console.log(`${this.id}\t${this.task}\t${this.complete ? "\t(complete)" : ""}`);
+  }
+}
+
+export default TodoItem;
--- 3/src/index.ts
+++ 3/src/index.ts
@@ -1,5 +1,12 @@
-console.log("hi");
+import TodoItem from "./Todoitem";
 
-const name1 = "hitit";
-let name2 = "gitit";
- 
+const data = [
+  { id: 1, task: "장보기", complete: true },
+  { id: 2, task: "TS 학습하기", complete: false },
+];
+
+console.log("My Todo List");
+for (let i = 0; i < data.length; i++) {
+  let todoItem = new TodoItem(data[i].id, data[i].task, data[i].complete);
+  todoItem.printDetails();
+}
