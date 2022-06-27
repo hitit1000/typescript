b86a0c7a36814c8173c47235413c48b956facfe6
hitit1000 <hitit1000@naver.com>
2022-06-27 14:51:57 +0900

    update 3_todolist:use map<> in TodoCollection.ts

--- 3/src/TodoCollection.ts
+++ 3/src/TodoCollection.ts
@@ -2,17 +2,34 @@ import TodoItem from "./Todoitem";
 
 class TodoCollection {
   private nextId: number = 1;
-  constructor(public userName: string, public todoItems: TodoItem[] = []) {}
+  private itemMap: Map<number, TodoItem>;
+  constructor(public userName: string, todoItems: TodoItem[] = []) {
+    this.itemMap = new Map<number, TodoItem>();
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
+  }
 
   getTodoById(id: number): TodoItem | undefined {
-    return this.todoItems.find((item) => item.id === id);
+    return this.itemMap.get(id);
   }
   addTodo(task: string): number {
     while (this.getTodoById(this.nextId)) {
       this.nextId++;
     }
-    this.todoItems.push(new TodoItem(this.nextId, task));
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
     return this.nextId;
+  }
+  // includeComplete => true : 모든 할일 목록
+  // includeComplete => false : 완료 목록은 제외한 할일 목록 반환
+  getTodoItems(includeComplete: boolean): TodoItem[] {
+    return [...this.itemMap.values()].filter((item) => includeComplete || !item.complete);
+  }
+
+  removeComplete(): void {
+    this.itemMap.forEach((item) => {
+      if (item.complete) {
+        this.itemMap.delete(item.id);
+      }
+    });
   }
   markComplete(id: number, complete: boolean): void {
     const todoItem = this.getTodoById(id);
--- 3/src/index.ts
+++ 3/src/index.ts
@@ -11,4 +11,14 @@ myTodoCollection.addTodo("react 학습하기");
 
 myTodoCollection.markComplete(3, true);
 console.log(`${myTodoCollection.userName}`);
-myTodoCollection.todoItems.forEach((item) => item.printDetails());
+// myTodoCollection.todoItems.forEach((item) => item.printDetails());
+myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+console.log("========================");
+myTodoCollection.getTodoItems(false).forEach((item) => item.printDetails());
+
+myTodoCollection.removeComplete();
+console.log("===========delete=============");
+
+myTodoCollection.getTodoItems(true).forEach((item) => item.printDetails());
+console.log("========================");
+myTodoCollection.getTodoItems(false).forEach((item) => item.printDetails());
